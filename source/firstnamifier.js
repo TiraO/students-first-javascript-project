ffc3e83facfa8dfd6794c9563f457f5a814ac098
Smaran Dayal <smaran@example.com>
2019-10-13 22:43:23 -0400

    make split not cheat (as much)

--- source/firstnamifier.js
+++ source/firstnamifier.js
@@ -1,5 +1,8 @@
 module.exports = class Firstnamifier {
-    split() {
-        return ["Smaran", "Eric"]
+    split(fullNames) {
+        let smaransNames = fullNames[0].split(" ")
+        let ericsNames = fullNames[1].split(" ")
+        let bothFirstNames = [smaransNames[0], ericsNames[0]]
+        return bothFirstNames
     }
 }
