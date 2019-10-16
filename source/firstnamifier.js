c820e40d382d3318b9c0a732efb5ad0ef988518a
Eric Iworsley <eiworsley@gmail.com>
2019-10-15 21:48:44 -0400

    make firstnamifier really not cheat

--- source/firstnamifier.js
+++ source/firstnamifier.js
@@ -1,8 +1,12 @@
 module.exports = class Firstnamifier {
     split(fullNames) {
-        let smaransNames = fullNames[0].split(" ");
-        let ericsNames = fullNames[1].split(" ");
-        let bothFirstNames = [smaransNames[0], ericsNames[0],];
-        return bothFirstNames;
+        let allFirstNames = []
+        fullNames.forEach((fullName) => {
+            let firstName = fullName.split(" ")[0]
+
+            allFirstNames.push(firstName)
+
+        })
+        return allFirstNames;
     }
 };
