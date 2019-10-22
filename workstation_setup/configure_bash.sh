0c2a272732607ab83be74883dd5964f04636d4ca
Tira Odhner <atiraodhner@gmail.com>
2019-10-21 21:45:44 -0400

    Add script for configuring bash

---
+++ workstation_setup/configure_bash.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/Bash-it/bash-it.git ~/.bash_it
+cp ./files_to_copy/bobby_pivotal.theme.bash ~/.bash_it/themes/bobby/bobby.theme.bash
+cp ./files_to_copy/.inputrc ~/
+~/.bash_it/install.sh --silent
+source ~/.bash_profile
+bash-it enable completion git
+bash-it enable plugin ssh
+bash-it enable completion ssh
+
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
