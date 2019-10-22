e515dc22cb170b50392b7d92400d4af0135e9d00
Eric Iworsley <eiworsley@gmail.com>
2019-10-21 21:55:45 -0400

    Fix bash configuration to set shortcuts on linux as well as mac

--- workstation_setup/configure_bash.sh
+++ workstation_setup/configure_bash.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 git clone https://github.com/Bash-it/bash-it.git ~/.bash_it
-cp ./bobby_pivotal.theme.bash ~/.bash_it/themes/bobby/bobby.theme.bash
-cp ./.inputrc ~/
+cp ./workstation_setup/bobby_pivotal.theme.bash ~/.bash_it/themes/bobby/bobby.theme.bash
+cp ./workstation_setup/.inputrc ~/
 ~/.bash_it/install.sh --silent
 source ~/.bash_profile
+source ~/.bashrc
 bash-it enable completion git
 bash-it enable plugin ssh
 bash-it enable completion ssh
