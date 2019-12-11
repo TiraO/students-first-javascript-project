f1b6ab6dd526a31a697cf6af20bd66590202fa24
Eric Iworsley <eiworsley@gmail.com>
2019-12-10 21:16:58 -0500

    make line.js print all birds and end

--- source/recursion/line.js
+++ source/recursion/line.js
@@ -1,16 +1,18 @@
 const LineSegment = require("./line_segment.js");
 
 function printLine(lineSegment) {
-    console.log(lineSegment.content);
-    console.log("************************");
-    console.log(lineSegment.next.content);
-    console.log("************************");
-    console.log(lineSegment.next.next.content);
+    if (lineSegment) {
+        console.log(lineSegment.content);
+        printLine(lineSegment.next);
+    } else {
+        console.log("End");
+    }
 }
+
 function makeLine(length) {
     let lineSegment;
-    for (let i=0; i<length; i++) {
-        lineSegment = new LineSegment("birds" +i, lineSegment);
+    for (let i = 0; i < length; i++) {
+        lineSegment = new LineSegment("birds" + i, lineSegment);
     }
     return lineSegment;
 }
