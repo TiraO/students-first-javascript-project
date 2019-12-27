6445e64912e534aed608e062bacd5f81d193b10c
Eric Iworsley <eiworsley@gmail.com>
2019-12-26 21:31:44 -0500

    wait until page load before code runs

--- source/on_click_behavior.js
+++ source/on_click_behavior.js
@@ -1,7 +1,7 @@
-alert(
-    "our script got included! celebrate!"
-)
-let elements = document.getElementsByTagName("button")
-console.log(elements)
-let button = elements.item(0)
-console.log(button)
+function pageReady(){
+    let elements = document.getElementsByTagName("button")
+    console.log(elements)
+    let button = elements[0]
+    console.log("button", button)
+}
+window.addEventListener('load', pageReady);
