2e12e1c2bdcfe3b7341bc7e55ec2cb0287d7e4be
Smaran Dayal <smaran@example.com>
2019-12-26 21:05:39 -0500

    Add JS file to index.html and many buttons

---
+++ source/on_click_behavior.js
@@ -0,0 +1,7 @@
+alert(
+    "our script got included! celebrate!"
+)
+let elements = document.getElementsByTagName("button")
+console.log(elements)
+let button = elements.item(0)
+console.log(button)
