cef500125be0eb55f793b26b2f09590ea596f300
Eric Iworsley <eiworsley@gmail.com>
2020-01-09 22:25:16 -0500

    start refacturing the options to live in more of a tree structure

--- source/on_click_behavior.js
+++ source/on_click_behavior.js
@@ -1,36 +1,43 @@
-
-
 class AdventureGame {
     constructor() {
         this.storyText = "Sam goes to the kitchen and sees a bowl of FrUiT. \n" +
-          "Option 1: Sam eats a pineapple.\n" +
-          "Option Pineapple: Sam looks for another snack.";
+            "Option 1: Sam eats a pineapple.\n" +
+            "Option Pineapple: Sam looks for another snack.";
         this.storyText2 = "Sam gets heartburn from eating an entire pineapple. \n" +
-          "Option 1: Sam goes to get an antacid. \n" +
-          "Option Pineapple: Sam dances off the heartburn.";
+            "Option 1: Sam goes to get an antacid. \n" +
+            "Option Pineapple: Sam dances off the heartburn.";
         this.storyText3 = "Sam finds a jar of peanutbutter. \n" +
             "Option 1: Sam makes a peanutbutter sandwich. \n" +
-            "Option Pineapple: Sam eats the entire jar of peanutbutter."
-        this.currentOptions = [{text:"option 1", next: this.storyText2}, {text: "OpTIon pIneAppLe", next: this.storyText3}];
+            "Option Pineapple: Sam eats the entire jar of peanutbutter.";
+        this.storyText4 = "Sam feels much better. \n" +
+            "Option 1: Sam goes for a run. \n" +
+            "Option Pineapple: Sam eats another pineapple";
+        this.options = {
+            storyText: "Sam goes to the kitchen and sees a bowl of FrUiT. \n" +
+                "Option 1: Sam eats a pineapple.\n" +
+                "Option Pineapple: Sam looks for another snack.",
+            next: [{storyText: this.storyText2, next: [], buttonText: "option 1"}]
+        };
+        this.currentOptions = this.options
     }
 
-    start(){
+    start() {
         this.render();
     }
 
-    render(){
+    render() {
         let targetElement = document.getElementById("game-content");
         targetElement.innerHTML = "";
 
         let storyElement = document.createElement("p");
-        storyElement.innerText = this.storyText;
+        storyElement.innerText = this.currentOptions.storyText;
         targetElement.append(storyElement);
 
-        this.currentOptions.forEach((option)=>{
+        this.currentOptions.next.forEach((option) => {
             let optionButton = document.createElement("button");
-            optionButton.innerText = option.text;
-            optionButton.onclick = ()=>{
-                this.storyText = option.next;
+            optionButton.innerText = option.buttonText;
+            optionButton.onclick = () => {
+                this.currentOptions = option;
                 this.render()
             };
             targetElement.append(optionButton);
@@ -39,16 +46,17 @@ class AdventureGame {
 }
 
 
-function pageReady(){
+function pageReady() {
     let elements = document.getElementsByTagName("button")
     console.log(elements);
     let button = elements[0];
     console.log("button", button);
-    button.onclick = function showAlert(){
+    button.onclick = function showAlert() {
         alert("something else")
     };
 
     let game = new AdventureGame();
     game.start();
 }
+
 window.addEventListener('load', pageReady);
