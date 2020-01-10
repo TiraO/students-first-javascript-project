b9085d5b53a782e46b8224ee08487438fe6573c3
Tira Odhner <atiraodhner@gmail.com>
2020-01-09 21:25:35 -0500

    Add story text and half of first branch.

--- source/on_click_behavior.js
+++ source/on_click_behavior.js
@@ -2,7 +2,13 @@
 
 class AdventureGame {
     constructor() {
-        this.currentOptions = [{text:"option 1"}, {text: "option B"}, {text: "OpTIon pIneAppLe"}];
+        this.storyText = "Sam goes to the kitchen and sees a bowl of FrUiT. \n" +
+          "Option 1: Sam eats a pineapple.\n" +
+          "Option Pineapple: Sam looks for another snack.";
+        this.currentOptions = [{text:"option 1"}, {text: "OpTIon pIneAppLe"}];
+        this.storyText2 = "Sam gets heartburn from eating an entire pineapple. \n" +
+          "Option 1: Sam goes to get an antacid. \n" +
+          "Option Pineapple: Sam dances off the heartburn.";
     }
 
     start(){
@@ -12,11 +18,17 @@ class AdventureGame {
     render(){
         let targetElement = document.getElementById("game-content");
         targetElement.innerHTML = "";
+
+        let storyElement = document.createElement("p");
+        storyElement.innerText = this.storyText;
+        targetElement.append(storyElement);
+
         this.currentOptions.forEach((option)=>{
             let optionButton = document.createElement("button");
             optionButton.innerText = option.text;
-            optionButton.onclick = function onChooseOption(){
-                // TODO
+            optionButton.onclick = ()=>{
+                this.storyText = this.storyText2;
+                this.render()
             };
             targetElement.append(optionButton);
         });
