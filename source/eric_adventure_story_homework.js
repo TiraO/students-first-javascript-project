620ecad16af511a5d6169488c60fe4b5c68c50c7
Eric Iworsley <eiworsley@gmail.com>
2020-01-14 19:51:59 -0500

    fix formatting a lot, remove instance of double storyText2

--- source/eric_adventure_story_homework.js
+++ source/eric_adventure_story_homework.js
@@ -1,38 +1,54 @@
 class AdventureGame {
     constructor() {
-        this.storyText = "Sam goes to the kitchen and sees a bowl of FrUiT. \n" +
-            "Option 1: Sam eats a pineapple.\n" +
-            "Option Pineapple: Sam looks for another snack.";
         this.storyText2 = "Sam gets heartburn from eating an entire pineapple. \n" +
             "Option 1: Sam goes to get an antacid. \n" +
             "Option Pineapple: Sam dances off the heartburn.";
         this.storyText3 = "Sam finds a jar of peanutbutter. \n" +
             "Option 1: Sam makes a peanutbutter sandwich. \n" +
             "Option Pineapple: Sam eats the entire jar of peanutbutter.";
-        this.storyText4 = "Sam feels much better. \n" +
-            "Option 1: Sam goes for a run. \n" +
-            "Option Pineapple: Sam eats another pineapple";
         this.storyText5 = "Sam can't decide how they want to cut the sandwich. \n" +
             "Option 1: Sam cuts it into squares. \n" +
             "Option Pineapple: Sam cuts it into triangles.";
-        this.storyText6 = "Sam feels invigorated after the run. \n" +
-            "Option 1: Sam cleans up around the apartment. \n" +
-            "Option Pineapple: Sam stays outside and enjoys a day in the park.";
         this.options = {
             storyText: "Sam goes to the kitchen and sees a bowl of FrUiT. \n" +
                 "Option 1: Sam eats a pineapple.\n" +
                 "Option Pineapple: Sam looks for another snack.",
-            next: [{
-                storyText: this.storyText2,
-                next: [{storyText: this.storyText2,
-                    next: [{storyText: this.storyText4,
-                        next: [{storyText: this.storyText6, next: [], buttonText: "option 1"}], buttonText: "option 1"}], buttonText: "option 1"}],
-                buttonText: "option 1"
-            },
-                {storyText: this.storyText3, next: [
-                        {storyText: this.storyText5, next: [], buttonText: "option pineapple"}
-                    ], buttonText: "option pineapple"}],
-                buttonText: "option pineapple"
+            buttonText: "option pineapple",
+            next: [
+                {
+                    storyText: "Sam gets heartburn from eating an entire pineapple. \n" +
+                            "Option 1: Sam goes to get an antacid. \n" +
+                        "Option Pineapple: Sam dances off the heartburn.",
+                    buttonText: "option 1",
+                    next: [
+                        {
+                            storyText: "Sam feels much better. \n" +
+                                "Option 1: Sam goes for a run. \n" +
+                                "Option Pineapple: Sam eats another pineapple",
+                            buttonText: "option 1",
+                            next: [
+                                {
+                                    storyText: "Sam feels invigorated after the run. \n" +
+                                        "Option 1: Sam cleans up around the apartment. \n" +
+                                        "Option Pineapple: Sam stays outside and enjoys a day in the park.",
+                                    buttonText: "option 1",
+                                    next: [],
+                                }
+                            ],
+                        }
+                    ],
+                },
+                {
+                    storyText: this.storyText3,
+                    buttonText: "option pineapple",
+                    next: [
+                        {
+                            storyText: this.storyText5, next: [],
+                            buttonText: "option pineapple"
+                        }
+                    ],
+                }
+            ],
         };
         this.currentOptions = this.options
     }
