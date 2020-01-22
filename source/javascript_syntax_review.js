eeb57ab8a749a4a9d1c9427b688f1f1b269d2c7c
Tira Odhner <atiraodhner@gmail.com>
2020-01-21 19:04:09 -0500

    Add a file with the review of javascript syntax.

---
+++ source/javascript_syntax_review.js
@@ -0,0 +1,44 @@
+// declare a variable
+let someVariable = 4;
+
+// Functions
+//
+// def some_function():
+//   asdasd
+
+function add(first, second){
+  return first + second
+}
+
+console.log("output", add(1, 2));
+
+// class Calculator:
+//   add(self, first, second):
+class Calculator {
+  constructor() {
+  }
+
+  add(first, second){
+    return first + second;
+  }
+
+  subtract(first, second){
+    return first - second;
+  }
+}
+
+let calculator = new Calculator();
+console.log(calculator.add(7, 19));
+
+let drinks = {
+  coffee: "A delicious drink for the morning",
+  tea: "Another drink for the morning"
+};
+
+console.log(drinks.coffee);
+console.log(drinks["tea"]);
+console.log(drinks.beer);
+
+console.log(undefined == null);
+console.log(undefined === null);
+
