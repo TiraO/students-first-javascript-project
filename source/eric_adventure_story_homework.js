bae6da075327cae74eecc4a183574482e3414a1f
Carolyn Stallard <carolyn@example.com>
2020-01-30 19:40:52 -0500

    complete refactoring options data structure

--- source/eric_adventure_story_homework.js
+++ source/eric_adventure_story_homework.js
@@ -7,56 +7,89 @@ class AdventureGame {
                 {
                     buttonText: "option 1",
                     optionText: "Option 1: Sam eats a pineapple.",
-                    storyText: "Sam gets heartburn from eating an entire pineapple. \n" +
-                        "Option 1: Sam goes to get an antacid. \n" +
-                        "Option Pineapple: Sam dances off the heartburn.\n" +
-                        "Option Kyle: Go home and go to sleep",
+                    storyText: "Sam gets heartburn from eating an entire pineapple. \n",
                     next: [
                         {
                             buttonText: "option 1",
-                            storyText: "Sam feels much better. \n" +
-                                "Option 1: Sam goes for a run. \n" +
-                                "Option Pineapple: Sam eats another pineapple",
+                            optionText: "Option 1: Sam goes to get an antacid.",
+                            storyText: "Sam feels much better. \n",
                             next: [
                                 {
                                     buttonText: "option 1",
-                                    storyText: "Sam feels invigorated after the run. \n" +
-                                        "Option 1: Sam cleans up around the apartment. \n" +
-                                        "Option Pineapple: Sam stays outside and enjoys a day in the park.",
-                                    next: [],
+                                    optionText: "Option 1: Sam goes for a run.",
+                                    storyText: "Sam feels invigorated after the run. \n",
+                                    next:
+                                        [
+                                            {
+                                                buttonText: "option 1",
+                                                optionText: "Option 1: Sam cleans up around the apartment.",
+                                            },
+                                            {
+                                                buttonText: "option pineapple",
+                                                optionText: "Option Pineapple: Sam stays outside and enjoys a day in the park.",
+                                            }
+                                        ],
+                                },
+                                {
+                                    buttonText: "option pineapple",
+                                    optionText:
+                                        "Option Pineapple: Sam eats another pineapple",
                                 }
                             ],
                         },
                         {
                             buttonText: "Option Pineapple",
-                            storyText: "Sam dances off the heartburn. \n" +
-                                "Option 1: Sam dances forever. \n" +
-                                "Option Pineapple: Sam sits in an easy chair.",
-                        },
+                            optionText:
+                                "Option Pineapple: Sam dances off the heartburn.",
+                            storyText:
+                                "Sam dances off the heartburn. \n",
+                            next:
+                                [
+                                    {
+                                        buttonText: "option 1",
+                                        optionText: "Option 1: Sam dances forever.",
+                                    },
+                                    {
+                                        buttonText: "option pineapple",
+                                        optionText: "Option Pineapple: Sam sits in an easy chair.",
+                                    }
+                                ]
+                        }
+                        ,
                         {
                             buttonText: "Option Kyle",
-                            storyText: "Kyle kills Sam and gets the best sleep he's ever had. \n",
+                            optionText:
+                                "Option Kyle: Go home and go to sleep",
+                            storyText:
+                                "Kyle kills Sam and gets the best sleep he's ever had. \n",
                         }
                     ],
                 },
                 {
                     buttonText: "option pineapple",
-                    optionText: "Option Pineapple: Sam looks for another snack.",
-                    storyText: "Sam finds a jar of peanutbutter. \n" +
-                        "Option 1: Sam makes a peanutbutter sandwich. \n" +
-                        "Option Pineapple: Sam eats the entire jar of peanutbutter.",
-                    next: [
-                        {
-                            buttonText: "option pineapple",
-                            storyText: "Sam can't decide how they want to cut the sandwich. \n" +
-                                "Option 1: Sam cuts it into squares. \n" +
-                                "Option Pineapple: Sam cuts it into triangles.",
-                            next: []
-                        }
-                    ],
+                    optionText:
+                        "Option Pineapple: Sam looks for another snack.",
+                    storyText:
+                        "Sam finds a jar of peanutbutter. \n",
+                    next:
+                        [
+                            {
+                                buttonText: "option 1",
+                                optionText: "Option 1: Sam makes a peanutbutter sandwich.",
+                            },
+                            {
+                                buttonText: "option pineapple",
+                                optionText: "Option Pineapple: Sam eats the entire jar of peanutbutter.",
+                                storyText: "Sam can't decide how they want to cut the sandwich. \n" +
+                                    "Option 1: Sam cuts it into squares. \n" +
+                                    "Option Pineapple: Sam cuts it into triangles.",
+                                next: []
+                            }
+                        ],
                 }
             ],
-        };
+        }
+        ;
         this.currentOptions = this.options
     }
 
