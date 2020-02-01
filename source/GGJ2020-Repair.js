3ca878ccc760c9d997dfef9c7f156ff9832ce877
Carolyn Stallard <carolyn@example.com>
2020-02-01 11:08:17 -0500

    create html file for GGJ2020

---
+++ source/GGJ2020-Repair.js
@@ -0,0 +1,13 @@
+class AdventureGame {
+    constructor() {
+        this.options = {
+            buttonText: "A",
+            storyText: "On 1 September 2019, Hurricane Dorian made landfall as a category 5 hurricane in The Bahamas with wind speeds in excess of 185mph, lashing The Bahamian islands of Great Abaco (pop. ~17,200) and Grand Bahama (pop. ~51,000). \n",
+            next: [
+                {
+                    buttonText: ""
+                }
+            ]
+        }
+    }
+}
---
+++ source/on_click_GGJ2020.js
@@ -0,0 +1,4 @@
+class AdventureGame {
+    constructor() {}
+
+}
