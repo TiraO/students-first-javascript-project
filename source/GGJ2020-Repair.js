8b8dc38bbfeaea8f1e0871452d07b1c3a583eaad
Carolyn Stallard <carolyn@example.com>
2020-02-02 13:00:30 -0500

    add more story text

--- source/GGJ2020-Repair.js
+++ source/GGJ2020-Repair.js
@@ -2,31 +2,71 @@ class AdventureGame {
     constructor() {
         this.options = {
             buttonText: "A",
-            storyText: "When you are ready to begin the adventure, choose a scene:\n",
+            storyText: "Choose a scene:",
+            storyPhoto: "watch-photo \n",
             next: [
                 {
                     buttonText: "A",
                     optionText: "A: A tiny town.",
-                    storyText: "Once upon a time, in a tiny town on the outskirts of a deep wood, there lived a watchmaker. Day after day he sat in his shop, his wiry and weathered fingers expertly gracing faces with freshly painted numbers, a chaotic chorus of hands chattering in time as he worked. \n",
+                    storyText: "Once upon a time, in a tiny town on the outskirts of a deep wood, there lived a watchmaker. Day after day he sat in his shop, his wiry and weathered fingers expertly gracing faces with freshly painted numbers, a chaotic chorus of hands chattering in time as he worked. What was he working on?\n",
                     next: [
                         {
                             buttonText: "A",
                             optionText: "A: Watch faces.",
-                            storyText: "The watchmaker worked tirelessly, the efforts of his labor showing in the fine detail of every watch produced. Not a soul in the town would ever forget the time. \n",
+                            storyText: "The watchmaker worked tirelessly, the efforts of his labor showing in the fine detail of every watch produced. In such a tiny town there was not a large demand for watchmaking; he had full reign over the industry, confident that no passing peddler's product could ever come close to matching his. \n",
+                            next: [
+                                {
+                                    buttonText: "Pride cometh before the fall.",
+                                    optionText: null,
+                                    storyText: "Years passed and the watchmaker continued his craft. The townspeople respected his work and never questioned its quality. \n",
+                                    next: [
+                                        {
+                                            buttonText:
+                                        }
+
+                                    ]
+                                },
+                                {
+                                    buttonText: "As you sow, so shall you reap.",
+                                    optionText: null,
+                                    storyText: "Years passed and the watchmaker continued his craft. The townspeople respected his work and never questioned its quality. The watchmaker lived a peaceful, satisfactory life. \n",
+                                    next: [
+                                        {
+                                            buttonText: "Yes.",
+                                            optionText: "Return to the beginning?",
+                                            storyText: "The End.",
+                                        }
+                                        {
+                                            buttonText: "No."
+                                            optionText: null,
+                                            storyText: null,
+                                        }
+                                    ]
+                                }
+                            ],
                         },
                         {
                             buttonText: "B",
                             optionText: "B: Watching faces.",
-                            storyText: "The watchmaker worked tirelessly, etching numbers expertly into each cheek, chin, and forehead. \n",
+                            storyText: "The watchmaker worked tirelessly, etching numbers expertly into each cheek, chin, and forehead. Such a small town didn't require many watchers, but each one had to be flawless. The safety of the town was at stake. \n",
                         }
                     ],
                 },
                 {
                     buttonText: "B",
                     optionText: "B: A sprawling city.",
-                    storyText: "Once upon a time, in a sprawling city on the planet Galar, there lived a watchmaker. Day after day he sat in his shop, his wiry and weathered fingers expertly gracing faces with freshly painted numbers, a chaotic chorus of hands chattering in time as he worked. \n",
+                    storyText: "Once upon a time, in a sprawling city on the planet Galar, there lived a watchmaker. Day after day he sat in his shop, his wiry and weathered fingers expertly gracing faces with freshly painted numbers, a chaotic chorus of hands chattering in time as he worked. What was he working on? \n",
                     next: [
-                        {},
+                        {
+                            buttonText: "A",
+                            optionText: "A: Watch faces.",
+                            storyText: "The watchmaker worked tirelessly, the efforts of his labor showing in the fine detail of every watch produced. In such a big city competitors were many, but craftmanship of his caliber was as rare as a glimpse of the planet's third moon. \n",
+                        },
+                        {
+                            buttonText: "B",
+                            optionText: "B: Watching faces.",
+                            storyText: "The watchmaker worked tirelessly, etching numbers expertly into each cheek, chin, and forehead. The city was vast; so must be his legion. \n",
+                        },
                     ]
                 }
             ]
@@ -38,6 +78,8 @@ class AdventureGame {
     render() {
         let gameContentElement = document.getElementById("game-content");
         gameContentElement.innerHTML = null;
+
+        let watchPhoto = document.getElementById("watch-photo");
 
         let descriptionElement = document.createElement("p");
         descriptionElement.innerText = this.options.storyText;
@@ -57,6 +99,11 @@ class AdventureGame {
                 this.render()
             };
         });
+        this.options.next.forEach((option) => {
+            let photoElement = document.createElement("photo");
+            photoElement.innerText = option.storyPhoto;
+            gameContentElement.appendChild(photoElement);
+        });
 
     }
 }
@@ -66,6 +113,8 @@ function pageReady() {
     startButton.onclick = () => {
         let introText = document.getElementById("intro");
         introText.innerHTML = null;
+        let jamPhoto = document.getElementById("jam-photo");
+        jamPhoto.innerHTML = null;
 
         let game = new AdventureGame();
         game.render();
