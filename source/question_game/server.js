34bc4fc0e174877fb3494e9ac00051baf4989c32
Tira Odhner <atiraodhner@gmail.com>
2020-05-12 14:04:00 -0400

    Quickly attempt to set up a database connection for the question game

--- source/question_game/server.js
+++ source/question_game/server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 const QuestionGame = require("./question_game");
+const { Pool, Client } = require('pg')
 var app = express();
 var bodyParser = require('body-parser');
 
@@ -25,29 +26,56 @@ const game = new QuestionGame({
 
 });
 
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+});
+
+const readGameFromDatabase = async () => {
+
+  const gameJson = (await pool.query("SELECT * from games order by id desc limit 1")).rows[0].game;
+  game.startingQuestion = JSON.parse(gameJson);
+
+}
+
+const writeGameToDatabase = async () => {
+  try {
+    await pool.query('BEGIN')
+    const lastId = (await client.query("SELECT id from games order by id limit 1")).rows[0].id;
+    let gameJson = JSON.stringify(game.startingQuestion);
+    const insertGame = 'INSERT INTO games(id, game) VALUES ($1, $2)';
+    await pool.query(insertGame, [lastId + 1, gameJson]);
+    await pool.query('COMMIT')
+  } catch (e) {
+    await pool.query('ROLLBACK');
+    throw e
+  }
+}
+
+readGameFromDatabase();
 app.use(express.static(__dirname + '/public'));
 
-app.get('/questions/:questionId', function ( request, response ) {
+app.get('/questions/:questionId', function (request, response) {
   let questionId = request.params.questionId;
   response.setHeader('Content-Type', 'application/json');
   let questionResult = game.getQuestionById(game.startingQuestion, questionId);
   response.send(JSON.stringify(questionResult));
 });
 
-app.get('/questions/:questionId/next/:trueOrFalse', function ( request, response ) {
+app.get('/questions/:questionId/next/:trueOrFalse', function (request, response) {
   let questionId = request.params.questionId;
   let userAnswer = request.params.trueOrFalse === "true";
   response.setHeader('Content-Type', 'application/json');
   response.send(game.getNextQuestion(questionId, userAnswer));
 });
 
-app.post('/questions/:wrongAnswerId/addAnswer', function ( request, response ) {
+app.post('/questions/:wrongAnswerId/addAnswer', function (request, response) {
   let wrongAnswerId = request.params.wrongAnswerId;
   let { question, answer, isQuestionTrueForAnswer, user } = request.body.data;
   response.setHeader('Content-Type', 'application/json');
   response.send(game.addAnswer(wrongAnswerId, question, answer, isQuestionTrueForAnswer, user));
   var startingQuestion = JSON.stringify(game.startingQuestion);
   console.log(startingQuestion)
+  writeGameToDatabase();
 });
 
 const PORT = process.env.PORT || 3000;
