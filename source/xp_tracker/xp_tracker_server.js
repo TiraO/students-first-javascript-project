e26fd26462a18bf1ab0f22d0b90812e068911e53
Matthew Curry <mwc@mwc.localdomain>
2020-05-12 21:02:16 -0400

    begin to add database to store student scores

--- source/xp_tracker/xp_tracker_server.js
+++ source/xp_tracker/xp_tracker_server.js
@@ -1,14 +1,26 @@
 let express = require('express');
+const { Pool, Client } = require('pg')
 let app = express();
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, function () {
     console.log('App listening on port ' + PORT);
 });
-let addScore = (person, score, description) => {
+
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL
+});
+
+let addScore = async (person, score, description) => {
     let assignment = {
         person: person,
         score: score,
         description: description,
     }
+    await pool.query('BEGIN')
+
+    await pool.query('COMMIT')
+
 };
+
+
