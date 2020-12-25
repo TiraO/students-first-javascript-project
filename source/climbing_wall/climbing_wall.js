d516c2b07942ed1f65770da66cf4e204d990f1f3
Tira Odhner <atiraodhner@gmail.com>
2020-12-24 23:50:11 -0500

    you can kinda select which limb to move.

--- source/climbing_wall/climbing_wall.js
+++ source/climbing_wall/climbing_wall.js
@@ -10,6 +10,43 @@ class ClimbingWall {
 
 }
 
+class ClimberControls {
+  constructor(spine) {
+    this.spine = spine;
+    this.selectedLimb = null;
+    this.selectableLimbs = ["rear-upper-arm", "front-upper-arm", "front-thigh", "rear-thigh"];
+  }
+
+  selectLimb(limbName) {
+    this.selectedLimb = limbName;
+  }
+
+  deselectLimb(){
+    this.selectedLimb = null;
+  }
+
+  moveLimb(mouse){
+    let armContainer = climber.slotContainers[climber.skeleton.findSlotIndex(this.selectedLimb)];
+    let shoulder = armContainer.getGlobalPosition({x: 0, y: 0});
+    let shoulderOffset = {
+      x: mouse.x - shoulder.x,
+      y: mouse.y - shoulder.y
+    }
+    // !! spine has backwards angles.
+    let shoulderAngleRadians = Math.atan2(shoulderOffset.y, shoulderOffset.x) - 4.36;
+    let armLength = 90;
+    let upperArm = climber.skeleton.findSlot(this.selectedLimb);
+    upperArm.bone.rotation = -1 * (shoulderAngleRadians * 180 / Math.PI);
+    let shoulderToTarget = distance(mouse, shoulder);
+    if (shoulderToTarget < armLength) {
+      let upperArmLength = armLength / 2;
+      let armBendRadians = Math.acos((shoulderToTarget / 2) / upperArmLength);
+      upperArm.bone.rotation += 2 * radiansToRotation(armBendRadians);
+      upperArm.bone.children[0].rotation = -2 * radiansToRotation(armBendRadians)
+    }
+  }
+}
+
 let initialize = () => {
   const app = new PIXI.Application();
   document.body.appendChild(app.view);
@@ -43,37 +80,50 @@ let initialize = () => {
 
     console.log("slots", climber.skeleton.slots.map(slot => slot.data.name));
     window.climber = climber;
+    climber.skeleton.findSlot("head").bone.scaleX = 0.5
+    climber.skeleton.findSlot("head").bone.scaleY = 0.5
+    let upperArm = climber.skeleton.findSlot("front-upper-arm");
+    upperArm.bone.rotation = -90;
+
+    let climberControls = new ClimberControls(climber);
+    climberControls.selectLimb("front-upper-arm");
 
     app.stage.on('pointermove', (event) => {
-      climber.skeleton.findSlot("head").bone.scaleX = 0.5
-      climber.skeleton.findSlot("head").bone.scaleY = 0.5
-      let upperArm = climber.skeleton.findSlot("front-upper-arm");
-      upperArm.bone.rotation = -90;
       let mouse = event.data.global;
-      console.log(mouse, "canvasClick")
-      let upperArmContainer = climber.slotContainers[climber.skeleton.findSlotIndex("front-upper-arm")];
-      let shoulder = upperArmContainer.getGlobalPosition({x: 0, y: 0});
-      console.log("upperArmContainer.getGlobalPosition({x: 0, y: 0})",)
-      let shoulderOffset = {
-        x: mouse.x - shoulder.x,
-        y: mouse.y - shoulder.y
+
+      if(climberControls.selectedLimb){
+        climberControls.moveLimb(mouse);
       }
-      // !! spine has backwards angles.
-      let shoulderAngleRadians = Math.atan2(shoulderOffset.y, shoulderOffset.x) - 4.36;
-      let armLength = 90;
-      upperArm.bone.rotation = - 1 *(shoulderAngleRadians * 180 / Math.PI);
-      let shoulderToTarget = distance(mouse, shoulder);
-      if(shoulderToTarget < armLength){
-        let upperArmLength = armLength / 2;
-        let armBendRadians = Math.acos((shoulderToTarget / 2) / upperArmLength);
-        upperArm.bone.rotation += 2* radiansToRotation(armBendRadians);
-        upperArm.bone.children[0].rotation = -2 * radiansToRotation(armBendRadians)
+    });
+
+    app.stage.on('pointertap', (event) => {
+      let mouse = event.data.global;
+      let hitLimb = getHitLimb(climber, mouse);
+      if (hitLimb) {
+        climberControls.selectLimb(hitLimb)
+      } else {
+        climberControls.deselectLimb();
       }
     });
   }
 
+
 }
-let distance = (pointA, pointB) =>{
+
+let getHitLimb = (climber, point) =>{
+  let hitSlotIndex = climber.slotContainers.findIndex((container)=>{
+    return distance(container.getGlobalPosition({x: 0, y: 0}), point) < 30;
+  })
+
+  if(hitSlotIndex > -1) {
+    let slotName = climber.skeleton.slots[hitSlotIndex].data.name;
+    console.log("hit ", slotName)
+    return slotName;
+  } else {
+    return null;
+  }
+}
+let distance = (pointA, pointB) => {
   return Math.sqrt(Math.pow(pointA.x - pointB.x, 2) + Math.pow(pointA.y - pointB.y, 2));
 }
 let radiansToRotation = (radians) => -1 * radians * 180 / Math.PI
