fcc0df9a1d8af8a5e66ab3d9509159a4645ea329
Eric Iworsley <eiworsley@gmail.com>
2021-02-05 21:57:38 -0500

    snowflake is centered and falls down full screen height

--- source/snowflakes/snowflakes.js
+++ source/snowflakes/snowflakes.js
@@ -1,17 +1,17 @@
 let frameRate = 60
 
 class Snowflake {
-  constructor(stage) {
+  constructor(stage, stageWidth, stageHeight) {
     this.stage = stage
     this.isFinished = false
     this.snowflakeContainer = new PIXI.Container();
     stage.addChild(this.snowflakeContainer);
     this.branches = []
     let branchCount = 0;
+    this.snowflakeContainer.position.x = stageWidth/2;
+    this.snowflakeContainer.position.y = stageHeight/2;
     while (branchCount < 12) {
       let branch = new PIXI.Graphics();
-      branch.position.x = stage.width/2;
-      branch.position.y = stage.height/2;
       branch.rotation = branchCount * 2 * Math.PI / 12;
       branch.lineStyle(2, 0xFFFFFF, 0.8);
       if (branchCount % 2 == 1) {
@@ -22,10 +22,12 @@ class Snowflake {
       branchCount = branchCount + 1
     }
     this.points = [];
+    this.stageWidth = stageWidth;
+    this.stageHeight = stageHeight;
   }
 
   addLine(event) {
-    let point = {x: event.offsetX - this.stage.width/2, y: event.offsetY - this.stage.height/2};
+    let point = {x: event.offsetX - this.stageWidth/2, y: event.offsetY - this.stageHeight/2};
     this.points.push(point);
     let previousPointIndex = (this.points.length - 2) % this.points.length;
     console.log("previousPointIndex", previousPointIndex)
@@ -58,8 +60,8 @@ class Snowflake {
   }
 
   renderFrame = () => {
-    let velocity = this.stage.height / 4
-    let snowflakeAtBottom = this.snowflakeContainer.position.y >= this.stage.height
+    let velocity = this.stageHeight / 8
+    let snowflakeAtBottom = this.snowflakeContainer.position.y >= this.stageHeight
     if (snowflakeAtBottom) {
       this.moveToTop();
     } else {
@@ -73,7 +75,7 @@ class Snowflake {
   }
 
   moveToTop = () => {
-    let snowflakeX = Math.random() * this.stage.width
+    let snowflakeX = Math.random() * this.stageWidth
     this.snowflakeContainer.position.y = 0
     this.snowflakeContainer.position.x = snowflakeX
   }
@@ -81,13 +83,14 @@ class Snowflake {
 
 }
 
+
 let initialize = () => {
   const app = new PIXI.Application({antialias: true, width: window.innerWidth, height: window.innerHeight});
 
 // The application will create a canvas element for you that you
 // can then insert into the DOM.
   document.body.appendChild(app.view);
-  let snowflake = new Snowflake(app.stage);
+  let snowflake = new Snowflake(app.stage, window.innerWidth, window.innerHeight);
 
   let previousClickTime = new Date().valueOf();
   let isDoubleClick = () => {
