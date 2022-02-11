e4efc050fbf0f948884b0d3ceb9ac6ed5fee0be5
angry-bear <69491668+angry-bear@users.noreply.github.com>
2022-02-10 22:47:46 -0500

    WIP: valentine is made of multiple snowflakes - currently app is broken

--- source/snowflakes/public/valentine.js
+++ source/snowflakes/public/valentine.js
@@ -1,28 +1,53 @@
 class Valentine {
   constructor(stage) {
     this.stage = stage
-    this.snowflake = new Snowflake(stage, window.innerWidth, window.innerHeight);
-    let heartTexture = PIXI.Texture.from("heartmask.png");
+    this.valentineContainer = new PIXI.Container();
+    stage.addChild(this.valentineContainer);
+    this.valentineParts = []
+    this.leftSnowflake = new Snowflake(stage);
+    this.leftSnowflake.snowflakeContainer.position.x = stageWidth / 4
+    this.leftSnowflake.snowflakeContainer.position.y = stageHeight / 4
+    this.valentineParts.push(this.leftSnowflake);
+    this.valentineContainer.addChild(this.leftSnowflake);
+
+    this.middleSnowflake = new Snowflake(stage);
+    this.middleSnowflake.snowflakeContainer.position.y = 3 * stageHeight / 4
+    this.valentineParts.push(this.middleSnowflake);
+    this.valentineContainer.addChild(this.middleSnowflake);
+
+    let heartTexture = PIXI.Texture.from("wideheartmask.png");
     let heartContainer = new PIXI.Sprite(heartTexture);
     stage.addChild(heartContainer);
     heartContainer.x = -stageWidth / 2
-    heartContainer.y = -stageHeight / 2
+    heartContainer.y = 0
     heartContainer.width = stageWidth
     heartContainer.height = stageHeight
-    this.snowflake.snowflakeContainer.addChild(heartContainer)
-    this.snowflake.snowflakeContainer.mask = heartContainer
-    this.points = this.snowflake.points
+    this.valentineContainer.addChild(heartContainer)
+    this.valentineContainer.snowflakeContainer.mask = heartContainer
+    this.points = this.middleSnowflake.points
   }
+
+  // this.valentineParts.forEach((part)=>{
+  //   addLine(event);
+  //   previewLine(event);
+  //   animate();
+  //
+  // })
+
   addLine(event) {
-    this.snowflake.addLine(event)
-  }
+    this.leftSnowflake.addLine(event)
+    this.middleSnowflake.addLine(event)
+}
   previewLine(event) {
-    this.snowflake.previewLine(event)
+    this.leftSnowflake.previewLine(event)
+    this.middleSnowflake.previewLine(event)
   }
   fillSnowflake() {
-    this.snowflake.fillSnowflake()
+    this.leftSnowflake.fillSnowflake()
+    this.middleSnowflake.fillSnowflake()
   }
   animate() {
-    this.snowflake.animate()
+    this.leftSnowflake.animate()
+    this.middleSnowflake.animate()
   }
 }
